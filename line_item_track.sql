18cc954f4e9939ead1889ab9947d86b49c2cc014
jessica kilby <jessicakilby@gmail.com>
2017-02-22 15:28:24 -0600

    added files. 1-5, and 8 done

---
+++ line_item_track.sql
@@ -0,0 +1,1 @@
+12. line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.
---
+++ line_items_per_invoice.sql
@@ -0,0 +1,1 @@
+11. line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
