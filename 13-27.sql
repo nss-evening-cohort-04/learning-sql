3139ec2a1be1955dcbed36d9ce3b7782ab312094
jessica kilby <jessicakilby@gmail.com>
2017-02-25 13:51:08 -0600

    finished up to 21

--- 13-27.sql
+++ 13-27.sql
@@ -86,7 +86,16 @@ FROM(
 
 #20. top_agent.sql: Which sales agent made the most in sales over all?
 
-
+SELECT MAX("SUM(Total)"), FullName 
+FROM(
+	SELECT SUM(Total),FirstName||" "||LastName AS FullName 
+	FROM(
+		SELECT * 
+		FROM(
+			SELECT CustomerID, SupportRepId AS EmployeeId FROM Customer) 
+		AS x JOIN Employee ON x.EmployeeId == Employee.EmployeeId) 
+	AS Y JOIN Invoice ON Invoice.CustomerId == y.CustomerId 
+	GROUP BY EmployeeId)
 
 #21. sales_agent_customer_count.sql: Provide a query that shows the count of customers assigned to each sales agent.
 
